42eab941d54561cb0bd0888e58efac29c5e16eca
Hubert COLE <hcole@ags-cloud.fr>
2025-11-05 19:07:11 +0100

    disable reload on sw update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
-import { Component, inject } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { SwUpdate } from '@angular/service-worker';
 
 @Component({
     selector: 'app-root',
@@ -9,25 +8,8 @@ import { SwUpdate } from '@angular/service-worker';
     imports: [RouterOutlet],
 })
 export class AppComponent {
-    private swUpdate = inject(SwUpdate);
-
     /**
      * Constructor
      */
-    constructor() {
-        if (this.swUpdate.isEnabled) {
-            // 1) Détecte puis active l’update et recharge
-            this.swUpdate.versionUpdates.subscribe(async (e) => {
-                // e.type === 'VERSION_READY' dans les nouvelles versions d’Angular
-                try {
-                    await this.swUpdate.activateUpdate();
-                } finally {
-                    document.location.reload();
-                }
-            });
-
-            // 2) Optionnel : check régulier
-            setInterval(() => this.swUpdate.checkForUpdate(), 60_000); // toutes les 60s
-        }
-    }
+    constructor() {}
 }
