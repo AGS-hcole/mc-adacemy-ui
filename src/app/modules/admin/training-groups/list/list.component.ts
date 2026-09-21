235e6b131f799964fb560fe837992004e71c9695
Hubert COLE <hcole@ags-cloud.fr>
2026-09-21 21:59:10 +0200

    Removing extra console log

--- src/app/modules/admin/training-groups/list/list.component.ts
+++ src/app/modules/admin/training-groups/list/list.component.ts
@@ -86,8 +86,6 @@ export class AdminTrainingGroupsListComponent implements OnInit, OnDestroy {
             (this._activatedRoute.snapshot.data['sites'] as Site[]) || [];
         this.loading = false;
 
-        console.log(this.trainingGroups);
-
         this.searchInputControl.valueChanges
             .pipe(takeUntil(this._unsubscribeAll), debounceTime(200))
             .subscribe(() => {
